5916e66dea84c0a22eca86d9bfe9f28d98d60d3a
unknown <ratiporn.pr@gmail.com>
2023-10-30 11:23:58 +0700

    fix: app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,30 +10,32 @@ import { payment } from "./apps/payment.ts";
 import { bookingRouter } from "./apps/bookingRouter.ts";
 import { roomAvaliable } from "./apps/roomAvaliable.ts";
 
-const app: Express = express();
-const port = 4000;
+const init = async () => {
+  const app: Express = express();
+  const port = 4000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use("/room", roomRouter);
-app.use("/auth", authRouter);
-app.use("/profile", profileRouter);
-app.use("/paymentMethod", paymentMethodRouter);
-app.use("/validUser", validUser);
-app.use("/booking", bookingRouter);
-app.use("/avaliable", roomAvaliable);
-app.use("/checkout", payment);
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use("/room", roomRouter);
+  app.use("/auth", authRouter);
+  app.use("/profile", profileRouter);
+  app.use("/paymentMethod", paymentMethodRouter);
+  app.use("/validUser", validUser);
+  app.use("/booking", bookingRouter);
+  app.use("/avaliable", roomAvaliable);
+  app.use("/checkout", payment);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("server is running");
-});
+  app.get("/", (req: Request, res: Response) => {
+    res.send("server is running");
+  });
 
-app.get("*", (req: Request, res: Response) => {
-  res.status(404).send("Not Found");
-});
+  app.get("*", (req: Request, res: Response) => {
+    res.status(404).send("Not Found");
+  });
 
-app.listen(port, () => {
-  console.log(`server is running port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`server is running port ${port}`);
+  });
+};
 
-export default app;
+init();
